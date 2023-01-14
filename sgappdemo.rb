a90e29b520754528cd62b126cf608335a6d02ed8
Beatrix <68532117+abeatrix@users.noreply.github.com>
2023-01-13 20:54:24 -0800

    Add missing env vars

--- sgappdemo.rb
+++ sgappdemo.rb
@@ -1,4 +1,5 @@
 class Sgappdemo < Formula
+    # version "0.0.0"
     desc "Code intelligence and search"
     license "Sourcegraph Enterprise License (portions licensed under Apache 2)"
     homepage "https://github.com/sourcegraph/sourcegraph"
@@ -9,13 +10,23 @@ class Sgappdemo < Formula
     depends_on "go" => :build
     
     def install
+        arch = nil
+        os = 'darwin'
+        if RUBY_PLATFORM.index('darwin') != nil
+            is_x86 = RUBY_PLATFORM.index("x86")
+            if is_x86 != nil
+                arch = 'x64'
+            else
+                arch = 'arm64'
+            end
+        end
+        ENV["GOARCH"] = "#{arch}"
+        ENV["ENTERPRISE"] = "1"
+        ENV["DEV_WEB_BUILDER"] = "esbuild yarn run build-web"
         ldflags = %W[
-      -s -w
-      -X github.com/sourcegraph/sourcegraph/internal/version.timestamp=$(date +%s)
-      -X github.com/sourcegraph/sourcegraph/internal/version.version=${VERSION-0.0.0+dev}
-      -X github.com/sourcegraph/sourcegraph/internal/conf/deploy.forceType=single-program
-    ]
+            -X github.com/sourcegraph/sourcegraph/internal/conf/deploy.forceType=single-program
+        ]
 
-        system "go", "build", *std_go_args(output: bin/"sgappdemo"), "github.com/sourcegraph/sourcegraph/enterprise/cmd/sourcegraph"
+        system "go", "build", *std_go_args(output: bin/"sgappdemo", ldflags: ldflags), "github.com/sourcegraph/sourcegraph/enterprise/cmd/sourcegraph"
     end
 end
